67ea8c2983edcc79f76cfe32b7cb4925a960d560
philipmclifton <me@philipmclifton.com>
2016-06-23 10:34:15 +0100

    Add multiple guzzle version support - > 5.3 is supported - up to 6.3 is supported.

--- src/Client.php
+++ src/Client.php
@@ -23,7 +23,7 @@ class Client
     /**
      * @var string
      */
-    const version = '1.0.0';
+    const version = '1.0.2';
 
     /**
      * Client constructor.
--- src/Endpoints/BootstrapEndpoint.php
+++ src/Endpoints/BootstrapEndpoint.php
@@ -5,7 +5,6 @@ use Wonde\ResultIterator;
 
 class BootstrapEndpoint
 {
-
     /**
      * @var string
      */
@@ -40,14 +39,23 @@ class BootstrapEndpoint
      */
     private function client()
     {
-        return new Client([
-            'defaults' => [
+        if ((float) Client::VERSION >= 6) {
+            return new Client([
                 'headers' => [
                     'Authorization' => 'Basic ' . base64_encode($this->token . ':'),
                     'User-Agent'    => 'wonde-php-client-' . \Wonde\Client::version
                 ]
-            ]
-        ]);
+            ]);
+        } else {
+            return new Client([
+                'defaults' => [
+                    'headers' => [
+                        'Authorization' => 'Basic ' . base64_encode($this->token . ':'),
+                        'User-Agent'    => 'wonde-php-client-' . \Wonde\Client::version
+                    ]
+                ]
+            ]);
+        }
     }
 
     /**
