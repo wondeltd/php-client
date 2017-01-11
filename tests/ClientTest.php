f1069b5114ce3a2421929e0c7249fad1963ef601
philipmclifton <me@philipmclifton.com>
2017-01-11 12:14:49 +0000

    #1061 - SIMS year photo bug

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,10 +1,15 @@
 <?php
 
-class ClientTest extends PHPUnit_Framework_TestCase {
+class ClientTest extends PHPUnit_Framework_TestCase
+{
 
     public function testCreateClient()
     {
-        $class = new \Wonde\Client('token');
-        $this->assertTrue($class instanceof \Wonde\Client);
+        //        $class = new \Wonde\Client('token');
+        //        $this->assertTrue($class instanceof \Wonde\Client);
+
+        $client       = new \Wonde\Client('TOKEN#########');
+        var_dump($client->schools->search([]));
+
     }
 }
