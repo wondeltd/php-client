8d9e2f4f77aad8dfebb4b5e7edda6b96f33bfae4
Dan Walker <dwalker@wonde.com>
2017-11-10 12:36:54 +0000

    Adds support for employee absences, historical groups and pre admissions

---
+++ src/Endpoints/GroupsHistorical.php
@@ -0,0 +1,9 @@
+<?php namespace Wonde\Endpoints;
+
+class GroupsHistorical extends BootstrapEndpoint
+{
+    /**
+     * @var string
+     */
+    public $uri = 'groups-historical/';
+}
---
+++ src/Endpoints/StudentsPreAdmission.php
@@ -0,0 +1,9 @@
+<?php namespace Wonde\Endpoints;
+
+class StudentsPreAdmission extends BootstrapEndpoint
+{
+    /**
+     * @var string
+     */
+    public $uri = 'students-pre-admission/';
+}
